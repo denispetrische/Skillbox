43262ef8b8a1562329c0e61ff0714c6ae6569698
denispetrische <95141776+denispetrische@users.noreply.github.com>
2022-01-26 21:10:58 +0300

    Add files via upload

---
+++ Skillbox Homework 2.1 ( Data types)/Skillbox Homework 2.1 ( Data types)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Skillbox_Homework_2._1___Data_types_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+        
+              string fullName = "Denis Petrische";      // переменная для имени и фамилии
+              int age = 22;                             // переменная для возраста
+              string email = "0298287677@mail.ru";      // переменная для электронной почты
+       
+              float programmingScore = 12.6f;           // выбираем тип float для баллов
+              float physicsScore = 4.4f;                // по всем
+              float mathScore = 13.3f;                  // предметам
+
+
+              // используем паттерн для удобства вывода
+
+              string pattern = "FullName: {0} \nAge: {1} \nEmail: {2} \nProgrammingScore: {3} \nPhysicsScore: {4} \nMathScore: {5}";
+
+            Console.Write(pattern,               // вывод переменных в консоль
+                          fullName,              
+                          age,
+                          email,
+                          programmingScore,
+                          physicsScore,
+                          mathScore);
+
+            Console.ReadKey();                   // ожидание нажатия клавиши
+        }
+    }
+}
---
+++ Skillbox Homework 2.2 (Score Counting)/Skillbox Homework 2.2 (Score Counting)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Skillbox_Homework_2._2__Score_Counting_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // переменная для суммы баллов по всем предметам 
+            int TotalScore = 0;
+
+            // просим ввести количество предметов
+            Console.Write("Введите количество предметов?");
+
+            // считываем данные и преобразуем их в int 
+            int SubjectsQuantity = Convert.ToInt32(Console.ReadLine()); 
+            
+            // цикл для ввода баллов по каждому из предметов
+            for(int i = 0; i < SubjectsQuantity; i++)
+            {
+                Console.Write($"Введите баллы по предмету № {i+1} ")  ;
+                TotalScore += Convert.ToInt32(Console.ReadLine());
+            }
+
+            //очистим консоль для корректного вывода результатов
+            Console.Clear();
+
+            // переменная для среднего арифметического
+            float ArithmeticalMean = 0;
+
+            //считаем среднее арифметическое
+            ArithmeticalMean = (float)TotalScore / (float)SubjectsQuantity;
+
+            // Реализуем условие вывода по нажатию на любую кнопку
+            Console.Write("Для вывода результатов нажмите любую клавишу");
+            Console.ReadKey();
+
+            //очистим консоль для корректного вывода результатов
+            Console.Clear();
+
+            Console.Write($"Среднее арифметическое: {ArithmeticalMean} \nСумма всех баллов: {TotalScore}");
+            Console.ReadKey();
+        }
+    }
+}
