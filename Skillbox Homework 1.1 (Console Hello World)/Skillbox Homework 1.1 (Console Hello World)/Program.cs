cad9f78b145e131a2b8cb98c2a608652de28e8f0
denispetrische <95141776+denispetrische@users.noreply.github.com>
2022-01-26 18:01:22 +0300

    Add files via upload

---
+++ Skillbox Homework 1.1 (Console Hello World)/Skillbox Homework 1.1 (Console Hello World)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; // подключение библиотеки System
+
+namespace Skillbox_Homework_1._1__Console_Hello_World_ // пространство имён
+{
+    class Program // класс 
+    {
+
+        /// <summary>
+        /// Основной метод
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) 
+        {
+
+            Console.WriteLine("Hello World!"); // вывод с переносом строки 
+            Console.ReadKey(); // ожидание нажатия клавиши
+
+        }
+    }
+}
---
+++ Skillbox Homework 1.2 ( Console Write Writeln Difference)/Skillbox Homework 1.2 ( Console Write Writeln Difference)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; // подключение библиотеки System
+
+namespace Skillbox_Homework_1._2___Console_Write_Writeln_Difference_ // пространство имён 
+{
+    class Program // класс
+    {
+        /// <summary>
+        /// Основной метод
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Hello"); // вывод в консоль без переноса строки 
+            Console.Write(" World"); 
+            Console.Write("!!!");
+
+            do // цикл do while 
+            {
+
+            } 
+            while (Console.ReadKey().Key != ConsoleKey.Enter); // не закончится пока не будет нажата 
+                                                               // клавиша Enter 
+        }
+    }
+}
