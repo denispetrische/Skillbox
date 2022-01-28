d10388ca3728475bb71c3120346572e070559841
denispetrische <95141776+denispetrische@users.noreply.github.com>
2022-01-28 21:13:40 +0300

    Add files via upload

---
+++ Skillbox Homework 5.1/Skillbox Homework 5.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Skillbox_Homework_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите несколько слов разделённых пробелами?");
+            string text = Console.ReadLine();
+
+            string[] separatorValue = new string[1];
+            separatorValue[0] = " ";
+
+            string[] formattedText = DivideString(text, separatorValue);
+            PrintSubstring(formattedText);
+
+            Console.ReadKey();          
+        }
+
+        /// <summary>
+        /// Возвращает массив, элементы которого являются подстроками исходной строки 
+        /// с удалёнными пустыми местами оставшимися от пробелов 
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <param name="divider"></param>
+        /// <returns></returns>
+        static string[] DivideString(string inputString, string[] divider)
+        {
+            return inputString.Split(divider, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Выводит каждую подстроку из массива
+        /// </summary>
+        /// <param name="text"></param>
+        static void PrintSubstring(string[] text)
+        {
+            foreach (var e in text)
+            {
+                Console.WriteLine($"{e}"); 
+            }
+        }
+    }
+}
---
+++ Skillbox Homework 5.2/Skillbox Homework 5.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Skillbox_Homework_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение разделённое пробелами");
+            string phrase = Console.ReadLine();
+
+            Console.WriteLine($"{ReversWords(phrase)}");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Вызывает метод разделения строки и переставляет подстроки c конца в начало.
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <returns></returns>
+        private static string ReversWords(string inputPhrase)
+        {
+            var formattedPhrase = DivideWords(inputPhrase);
+
+            string reversedPhrase = "";
+
+            for (int i = 0; i < formattedPhrase.Length; i++)
+            {
+                reversedPhrase += formattedPhrase[formattedPhrase.Length - (1 + i)] + " ";
+            }
+
+            return reversedPhrase;
+        }
+
+        /// <summary>
+        /// Разделяет входную строку по символам пробела " " и возвращает в виде массива подстрок.
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <returns></returns>
+        private static string[] DivideWords(string inputPhrase)
+        {
+            string[] divider = new string[1];
+            divider[0] = " ";
+
+            return inputPhrase.Split(divider, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
