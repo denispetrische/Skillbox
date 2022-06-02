00c3816a2ef47435eec01883ad0049efd6408f1f
denispetrische <95141776+denispetrische@users.noreply.github.com>
2022-06-02 11:31:08 +0300

    Add files via upload

---
+++ Skillbox Homework 10.1/Skillbox Homework 10.1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Media;
+using System.Text.Json;
+using System.IO;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+
+namespace Skillbox_Homework_10._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TelegramMessageClient client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(KeyPressedDown);
+            client = new TelegramMessageClient(this, "");
+
+            listView.ItemsSource = client.BotMessage;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBlock.Text != "")
+            {
+                client.SendMessage(textBox.Text, textBlock.Text);
+                textBox.Text = "";
+            }
+        }
+
+        private void ButtonSave(object sender, RoutedEventArgs e)
+        {
+            var json = JsonConvert.SerializeObject(client.BotMessage);
+
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText($@"{dialog.FileName}", json);
+            }           
+        }
+
+        private void ButtonLoad(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                string foo = File.ReadAllText(dialog.FileName);
+                client.BotMessage.Clear();
+
+                var json = JsonConvert.DeserializeObject<List<MessageLog>>(foo);
+
+                foreach (dynamic item in json)
+                {
+                    client.BotMessage.Add(new MessageLog(item.time, item.id, item.message, item.firstName));
+                }
+            }
+        }
+
+        private void KeyPressedDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (textBlock.Text != "")
+                {
+                    client.SendMessage(textBox.Text, textBlock.Text);
+                    textBox.Text = "";
+                }
+            }
+        }
+    }
+}
---
+++ Skillbox Homework 10.1/Skillbox Homework 10.1/MessageLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skillbox_Homework_10._1
+{
+    class MessageLog
+    {
+        public string time { get; set; }
+
+        public string id { get; set; }
+
+        public string message { get; set; }
+
+        public string firstName { get; set; }
+
+        public MessageLog(string time, string id, string message, string firstName)
+        {
+            this.time = time;
+            this.id = id;
+            this.message = message;
+            this.firstName = firstName;
+        }
+    }
+}
