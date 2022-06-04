773778cd81ba146fca0b7a2a376b6cde7540f314
denispetrische <95141776+denispetrische@users.noreply.github.com>
2022-06-04 19:44:07 +0300

    Add files via upload

---
+++ Skillbox Homework 11.1/Skillbox Homework 11.1/Undefined.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillbox_Homework_11._1
+{
+    public class Undefined : User
+    {
+        public Undefined()
+        {
+            isSecondNamePermittedRead = false;
+            isFirstNamePermittedRead = false;
+            isPatronymicPermittedRead = false;
+            isPhoneNumberPermittedRead = false;
+            isPassportPermittedRead = false;
+
+            isSecondNamePermittedWrite = false;
+            isFirstNamePermittedWrite = false;
+            isPatronymicPermittedWrite = false;
+            isPhoneNumberPermittedWrite = false;
+            isPassportPermittedWrite = false;
+        }
+    }
+}
